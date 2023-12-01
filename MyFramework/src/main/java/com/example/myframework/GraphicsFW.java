d86f151ca496e70d2b276881bdcff508813c8c52
Miha <Liverpoolfc291892@gmail.com>
2023-12-01 12:26:46 +0200

    add CoreClass step_1

---
+++ MyFramework/src/main/java/com/example/myframework/GraphicsFW.java
@@ -0,0 +1,68 @@
+package com.example.myframework;
+
+import android.accounts.AccountManager;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+
+public class GraphicsFW {
+
+    // для работы с файлами
+    private AssetManager assetManagerGame;
+    // для работы сграфикой, будет подстраивать размеры графики под различные велиыины экранов.
+    private Bitmap frameBufferGame;
+    // для приема графики
+    private Canvas canvasGame;
+    // для работы с графикой
+    private Paint paintGame;
+    // для текстур
+    private Bitmap textureGame;
+
+    public GraphicsFW(AssetManager assetManagerGame, Bitmap frameBufferGame) {
+        this.assetManagerGame = assetManagerGame;
+        this.frameBufferGame = frameBufferGame;
+        // передаем туда фреймБуфер, не рисуем на самом холсте канвас.
+        this.canvasGame = new Canvas(frameBufferGame);
+        this.paintGame = new Paint();
+    }
+
+    // для очистки т.е. закраски фреймбуфера данным цветом.
+    public void clearScene(int colorRGB) {
+        canvasGame.drawRGB(colorRGB, colorRGB, colorRGB); // принимает три параметра R G B но мы передаем одинаковые значения что бы получить один цвет
+    }
+
+    public void drawPixel(int x, int y, int color) {
+        paintGame.setColor(color);
+        canvasGame.drawPoint(x, y, paintGame);
+    }
+
+    public void drawLine(int startX, int startY, int stopX, int stopY, int color) {
+        paintGame.setColor(color);
+        canvasGame.drawLine(startX, startY, stopX, stopY, paintGame);
+    }
+
+    public void drawText(String text, int x, int y, int color, int sizeText, Typeface font) {
+        paintGame.setColor(color);
+        paintGame.setTextSize(sizeText);
+        paintGame.setTypeface(font);
+        canvasGame.drawText(text, x, y, paintGame);
+    }
+
+    public void drawTexture(Bitmap textureGame, int x, int y) {
+        canvasGame.drawBitmap(textureGame, x, y, null);
+    }
+
+    //получение ширины фреймбуфера
+    public int getWightFrameBuffer() {
+        return frameBufferGame.getWidth();
+    }
+
+    // высоты фреймбуфера
+    public int getHeightFrameBuffer() {
+        return frameBufferGame.getHeight();
+    }
+
+
+}
