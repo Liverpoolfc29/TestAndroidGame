2654e5ea0a444faaffcf4c9834a01b206ae974e1
Miha <Liverpoolfc291892@gmail.com>
2023-12-08 16:36:46 +0200

    Creating main manu Step_2_1

--- app/src/main/java/com/example/test_1/Scenes/MainManuScene.java
+++ app/src/main/java/com/example/test_1/Scenes/MainManuScene.java
@@ -20,7 +20,11 @@ public class MainManuScene extends SceneFW {
     @Override
     public void drawing() {
         graphicsFW.clearScene(Color.BLACK);
-        // обращаемся к нашему файлу strings.xml. Каждая сцена работает с ядром и общараясь к ядру должны видеть их.
+        /*
+         обращаемся к нашему файлу strings.xml. Каждая сцена работает с ядром и общараясь к ядру должны видеть их.
+         B данном месте не важно какой размер экрана, мы все ресуем на фреймбуфер который всегда у нас размером 800 на 600.
+         На каждой итерации Лупа фрейм буфер будет менятся и по идее масштабировать под экран смартфона на котором запущено приложение.
+        */
         graphicsFW.drawText(coreFW.getString(R.string.txt_mainManu_nameGame), 100, 100, Color.BLUE, 60, null);
         graphicsFW.drawText(coreFW.getString(R.string.txt_mainManu_newGame), 20, 300, Color.BLUE, 40, null);
         graphicsFW.drawText(coreFW.getString(R.string.txt_mainManu_settings), 20, 350, Color.BLUE, 40, null);
