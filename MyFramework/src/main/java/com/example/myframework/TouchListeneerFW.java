a4fc7cce55316d2bdb42f61482e60cf56d5f1786
Miha <Liverpoolfc291892@gmail.com>
2023-12-11 10:56:56 +0200

    Creating main manu Step_2_2

---
+++ MyFramework/src/main/java/com/example/myframework/TouchListeneerFW.java
@@ -0,0 +1,13 @@
+package com.example.myframework;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+public class TouchListeneerFW implements View.OnTouchListener {
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+
+}
